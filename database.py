ffd415b165394582838b53cc754691bb103d2975
Neptune Michael Cabelin <ncabelin@gmail.com>
2016-12-05 15:35:07 -0800

    refactor Test table

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
